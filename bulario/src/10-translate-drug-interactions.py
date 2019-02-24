d4609b73b808d2146133232173b3a00370cac2b0
altierispeixoto <altieris.marcelino@gmail.com>
2019-02-24 08:18:15 -0300

    translate drug interactions

--- bulario/src/10-translate-drug-interactions.py
+++ bulario/src/10-translate-drug-interactions.py
@@ -1,7 +1,7 @@
-import pandas as pd
-from googletrans import Translator
 import requests
-
+import pandas as pd
+import multiprocessing
+from googletrans import Translator
 
 def get_proxy_list():
     url_proxy = 'https://free-proxy-list.net/'
@@ -27,7 +27,7 @@ def translate(text, src, dest, hotproxy):
                 if hotproxy == '':
                     hotproxy = proxy
 
-                translator = Translator(proxies={'https': hotproxy}, timeout=5)
+                translator = Translator(proxies={'http': hotproxy}, timeout=5)
                 t = translator.translate(text, dest=dest, src=src)
 
                 if t.text != '':
@@ -49,24 +49,55 @@ drug_interactions_filtered = drug_interations[(drug_interations.drugbank_id.isin
 drug_interactions_filtered = drug_interactions_filtered[(drug_interactions_filtered.drug_interaction_id.isin(approved_drugs.drugbank_id))]
 
 
+interactions_translated = pd.read_csv('/data/drugbank/processed/drugbank-interactions-translated.tsv',sep='\t',names=['drug_interaction_id','name','description','drugbank_id','name_pt_br','description_pt_br'])
+
+print(interactions_translated.shape)
+
+drug_interactions_filtered = drug_interactions_filtered[~(drug_interactions_filtered.drug_interaction_id.isin(interactions_translated.drug_interaction_id) & drug_interactions_filtered.drugbank_id.isin(interactions_translated.drugbank_id))]
+
+print(drug_interactions_filtered.shape)
+
 drug_interactions_filtered = drug_interactions_filtered.fillna('')
-drug = dict()
-hotproxy = ''
-for index, drug_interaction in drug_interactions_filtered.iterrows():
 
-    drug['drug_interaction_id'] = drug_interaction['drug_interaction_id']
-    drug['name'] = drug_interaction['name']
-    drug['description'] = drug_interaction['description']
-    drug['drugbank_id'] = drug_interaction['drugbank_id']
+drug_interactions_filtered = drug_interactions_filtered.reset_index()
 
-    hotproxy, drug['name_pt_br'] = translate(drug['name'], 'en', 'pt', hotproxy)
 
-    hotproxy, drug['description_pt_br'] = translate(drug['description'], 'en', 'pt', hotproxy)
+# create as many processes as there are CPUs on your machine
+num_processes = multiprocessing.cpu_count()
 
-    print(drug)
-    #print('{} - {} '.format(drug['name'], drug['name_pt_br']))
+# calculate the chunk size as an integer
+chunk_size = int(drug_interactions_filtered.shape[0]/num_processes)
 
-    drug_df = pd.DataFrame(drug, index=[0])
+# this solution was reworked from the above link.
+# will work even if the length of the dataframe is not evenly divisible by num_processes
+chunks = [drug_interactions_filtered.iloc[drug_interactions_filtered.index[i:i + chunk_size]] for i in range(0, drug_interactions_filtered.shape[0], chunk_size)]
 
-    drug_df.to_csv('/data/drugbank/processed/drugbank-interactions-translated.tsv', header=False, sep='\t', index=False, mode='a')
+def func(d):
+    drug = dict()
+    hotproxy = ''
 
+    for index, row in d.iterrows():
+
+        drug['drug_interaction_id'] = row['drug_interaction_id']
+        drug['name'] = row['name']
+        drug['description'] = row['description']
+        drug['drugbank_id'] = row['drugbank_id']
+
+        hotproxy, drug['name_pt_br'] = translate(drug['name'], 'en', 'pt', hotproxy)
+
+        hotproxy, drug['description_pt_br'] = translate(drug['description'], 'en', 'pt', hotproxy)
+
+        print(drug)
+
+        drug_df = pd.DataFrame(drug, index=[0])
+
+        drug_df.to_csv('/data/drugbank/processed/drugbank-interactions-translated.tsv', header=False, sep='\t',index=False, mode='a')
+
+    return d
+
+
+# create our pool with `num_processes` processes
+pool = multiprocessing.Pool(processes=num_processes)
+
+# apply our function to each chunk in the list
+result = pool.map(func, chunks)
